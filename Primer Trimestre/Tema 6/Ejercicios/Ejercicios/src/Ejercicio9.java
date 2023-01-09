7fd9c8257ef18ecd8a2535415e89429a2c6e3d53
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-09 18:36:15 +0300

    Ejercicio9

--- Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio9.java
+++ Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio9.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Ejercicio9 {
     public static void main(String[] args) {
-        
+
+        Scanner leer = new Scanner(System.in);
+        int array100Num[]=new int[100], numUsuario, posicion=0;
+        System.out.print("Introduce un número: ");
+        numUsuario = leer.nextInt();
+        for (int i = 0; i < array100Num.length; i++) {
+            array100Num[i]= (int) (Math.random()*(10-1)+1);
+        }
+        for (int i = 0; i < array100Num.length; i++) {
+            if (numUsuario==array100Num[i]) {
+                posicion = i;
+                System.out.println("El valor "+numUsuario+" esta en la posicion "+posicion);
+            }
+        }
+    leer.close();
     }
 }
