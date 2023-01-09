2aefe9b70f18f02b9dcd6def189436ddd4a9bfc6
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-09 19:14:39 +0300

    Comienzo del ejercicio 10

--- Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio10.java
+++ Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio10.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Ejercicio10 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        int altura[], personas;
+        
+        entrada.close();
+    }
 }
