1f081687c0aae1a7486841612f735cc9c2a446b4
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-10 16:13:28 +0300

    Ejercicio 10 terminado

--- Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio10.java
+++ Primer Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio10.java
@@ -1,11 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ejercicio10 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-
-        int altura[], personas;
+        System.out.print("¿De cuantas personas quieres almacenar la altura?");
+        int personas = entrada.nextInt();
+        int altura[] = new int[personas];
+        int media=0;
         
+        for(int i=0; i<altura.length; i++){
+            System.out.print("Añade una altura: ");
+            altura[i] = entrada.nextInt();
+        }
+        Arrays.sort(altura);
+        System.out.println("La altura más pequeña es: " + altura[0]);
+		System.out.println("La altura más grande es:  " + altura[altura.length - 1]);
+        for(int i=0; i<altura.length; i++){
+            media = media + altura[i];
+        }
+        media = media/altura.length;
+        System.out.println("La media de altura es: "+ media);
+
+
         entrada.close();
     }
 }
