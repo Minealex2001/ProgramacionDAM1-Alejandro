ba30c1c590998375e8235c594ad8bc2d60145e9a
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-11 18:18:40 +0300

    Codigo prefecto incluso mejorado

--- Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
+++ Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
@@ -6,9 +6,9 @@ public class EjercicioObligatorio {
         Scanner entrada = new Scanner(System.in);
         String nombre[] = new String[8];
         System.arraycopy(nomalu(entrada), 0, nombre, 0, 8);
-
-        char respuestas[][] = new char[8][numpregun(entrada)];
-        System.arraycopy(notas(entrada), 0, respuestas, 0, respuestas[].length);
+        int numpreguntas = numPregun(entrada);
+        char respuestas[][] = new char[8][numpreguntas];
+        System.arraycopy(notas(entrada, numpreguntas, nombre), 0, respuestas, 0, numpreguntas);
 
         
         //Muestro el menu
@@ -29,12 +29,25 @@ public class EjercicioObligatorio {
             return alumno;
     }
 
-    public static int numpregun(Scanner entrada) {
+    public static int numPregun(Scanner entrada) {
         int numero = 0;
         while(numero<=0){
         System.out.print("¿Cuantas preguntas tiene el examen?");
         numero = entrada.nextInt();
         }
         return numero;
+    }
+
+    public static char[][] notas(Scanner entrada, int numero, String nombre[]) {
+        char respuestas[][] = new char[8][numero];
+        for(int i=0; i < 8; i++){
+            System.out.print("Introduce las respuestas para el alumno "+ nombre[i]);
+            for(int j=0;j<numero;j++){
+                System.out.print("Introduce la respuesta para le pregunta " + j);
+                String resultado = entrada.nextLine();
+                respuestas[i][j] = resultado.charAt(0); 
+            }
+        }
+        return respuestas;
     }
 }
