437938d27212f69526a0d82b9f4e8858dadc24e3
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-11 15:01:29 +0300

    Cambio del tema6 a el segundotri y creacion ejobli

--- Segundo Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio16.java
+++ Segundo Trimestre/Tema 6/Ejercicios/Ejercicios/src/Ejercicio16.java
@@ -4,10 +4,11 @@ public class Ejercicio16 {
     
     public static void main(String[] args) {
 
-        int array[] = new int[55], control=0;
+        int array[] = new int[55], control=1,control2=0;
         for(int i=1; i<=10; i++){
-            for(int u=0; u <=i;u++){
-                Arrays.fill(array, i, control, u+1);
+            control2 = control2+i;
+            while(control<=i){
+                Arrays.fill(array, control, control2, i);
                 control++;
             }
         }
