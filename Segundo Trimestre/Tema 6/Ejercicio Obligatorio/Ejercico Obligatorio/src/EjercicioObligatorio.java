24ecf34f32b8a291669af5b241b02b622d53918e
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-12 19:08:46 +0300

    Esto es una obra maestra

--- Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
+++ Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EjercicioObligatorio {
@@ -22,13 +23,11 @@ public class EjercicioObligatorio {
          */
         System.arraycopy(respuestaalumno(entrada, numpreguntas, nombre), 0, respuestas, 0, numpreguntas);
 
-        
-        //Muestro el menu
-        System.out.println("Opción 1 Notas: Muestra la nota obtenida de cada estudiante. Esta nota corresponde al número de aciertos que tuvo el estudiante.");
-        System.out.println("Opción 2 Media: Muestra la media de aciertos de los 8 alumnos.");
-        System.out.println("Opción 3 Nota Alumno: dado un nombre de un alumno, muestra su nota.");
-        System.out.println("Opción 4 Pregunta: Muestra el número de la pregunta más acertada");
-        
+        char respuestascorrectas[] = new char[numpreguntas];
+        System.arraycopy(respuestacorrecta(entrada, numpreguntas), 0, respuestascorrectas, 0, numpreguntas);
+
+
+        menu(numpreguntas, nombre, respuestascorrectas, respuestas, entrada, numpreguntas);
         entrada.close();
     }
 
@@ -65,7 +64,7 @@ public class EjercicioObligatorio {
 
             //Este if solo es para mostrar un mensaje de error cuando el numero no cumpla con lo requerido.
             if(numero<=0){
-                System.err.println("El numero de preguntas no puede ser menor o igual a 0.");
+                System.err.println("[ERROR] El numero de preguntas no puede ser menor o igual a 0.");
             }
         }
         
@@ -92,23 +91,93 @@ public class EjercicioObligatorio {
             //Hago un bucle para ir cambiando las preguntas del cual se quieren almacenar las respuestas.
             for(int j=0;j<numpreguntas;j++){
 
-                entrada.nextLine();
                 System.out.print("Introduce la respuesta para la pregunta " + j + ": ");
                 String resultado = entrada.nextLine();
-
+                entrada.nextLine();
 //while(resultado.equalsIgnoreCase("A")||resultado.equalsIgnoreCase("B")||resultado.equalsIgnoreCase("C")||resultado.equalsIgnoreCase("D")||resultado.equalsIgnoreCase("E")){
 
                 /*Como metodo de control, realizo un while que estara siempre dando vueltas mientras el valor de resultado
                 * sea distinto de A, B, C, D o E, ya que no hay más respuestas aceptables.
                 */
-                while(!resultado.matches("[a-e]")){
-                    System.err.println("La respuesta tiene que estar entre la A y la E");
+                while(!resultado.matches("[A-E]")){
+                    System.err.println("[ERROR] La respuesta tiene que estar entre la A y la E");
                     System.out.print("Introduce la respuesta para la pregunta " + j + ": ");
                     resultado = entrada.nextLine();
                 }
+                /*Al haber leido un array en el escaner anterior, uso esta linea para sacar el primer
+                * caracter para añadirlo en el array de char para las respuestas de los alumnos.
+                */
                 respuestas[i][j] = resultado.charAt(0);
             }
         }
         return respuestas;
+    }
+
+    public static char[] respuestacorrecta(Scanner entrada, int numpreguntas) {
+        char respuestas[] = new char[numpreguntas];
+
+        for(int i=0; i < numpreguntas;i++){
+            System.out.print("Escribe la respuesta para la pregunta " + i + " ");
+            String resultado = entrada.nextLine();
+
+//while(resultado.equalsIgnoreCase("A")||resultado.equalsIgnoreCase("B")||resultado.equalsIgnoreCase("C")||resultado.equalsIgnoreCase("D")||resultado.equalsIgnoreCase("E")){
+
+            /*Como metodo de control, realizo un while que estara siempre dando vueltas mientras el valor de resultado
+            * sea distinto de A, B, C, D o E, ya que no hay más respuestas aceptables.
+            */
+            while(!resultado.matches("[A-E]")){
+                System.err.println("[ERROR] La respuesta tiene que estar entre la A y la E");
+                System.out.print("Escribe la respuesta para la pregunta " + i + " ");
+                resultado = entrada.nextLine();
+            }
+            /*Al haber leido un array en el escaner anterior, uso esta linea para sacar el primer
+            * caracter para añadirlo en el array de char para las respuestas correctas.
+            */
+            respuestas[i] = resultado.charAt(0);
+        }
+        return respuestas;
+    }
+
+    public static void menu( String nombre[], char respuestascorrectas[], char respuestaalumno[][], Scanner entrada, int numpreguntas ) {
+            //Muestro el menu.
+            System.out.println("Opción 1 Notas: Muestra la nota obtenida de cada estudiante. Esta nota corresponde al número de aciertos que tuvo el estudiante.");
+            System.out.println("Opción 2 Media: Muestra la media de aciertos de los 8 alumnos.");
+            System.out.println("Opción 3 Nota Alumno: dado un nombre de un alumno, muestra su nota.");
+            System.out.println("Opción 4 Pregunta: Muestra el número de la pregunta más acertada");
+            
+            String res = entrada.nextLine();
+            switch(res){
+                case "1":
+                    notastotales(nombre, respuestascorrectas, respuestaalumno, numpreguntas);
+            }
+    }
+
+    public static void notastotales(String nombre[], char respuestascorrectas[], char respuestaalumno[][], int numpreguntas) {
+
+        int nota[] = new int[8];
+
+        Arrays.fill(nota, 0);
+
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < numpreguntas ; j++){
+                if(respuestaalumno[i][j] == respuestascorrectas[i]){
+                    nota[i] = nota[i] + 1;
+                }
+            }
+        System.out.println("La nota del alumno " + nombre[i] + " es: " + nota[i]);
+        }
+    }
+    public static void media(char respuestascorrectas[], char respuestaalumno[][], int numpreguntas) {
+        int nota = 0;
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < numpreguntas ; j++){
+                if(respuestaalumno[i][j] == respuestascorrectas[i]){
+                    nota = nota + 1;
+                }
+            }
+        }
+        System.out.println("La nota media de la clase es: " + (nota/8));
+        
+        menu(null, respuestascorrectas, respuestaalumno, null, numpreguntas);
     }
 }
