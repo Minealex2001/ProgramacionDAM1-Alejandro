7eb120e7aa2591ca757bb230df596e095467a8b0
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-01-12 01:27:10 +0100

    Añadidos comentarios y comenzado otro metodo

--- Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
+++ Segundo Trimestre/Tema 6/Ejercicio Obligatorio/Ejercico Obligatorio/src/EjercicioObligatorio.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 
 public class EjercicioObligatorio {
     public static void main(String[] args) {
-
+        //Inicializo el escaner.
         Scanner entrada = new Scanner(System.in);
+
+        //Declaro la variable que utilizare para almacenar los nombres.
         String nombre[] = new String[8];
+
+        /* Aqui utilizo un system.arraycopy para copiar el array que me dara como resultado el metodo que llamo
+         * dentro de la misma linea "nomalu(entrada)" y lo almaceno en el array previamente declarado e instanciado "nombre".
+         */
         System.arraycopy(nomalu(entrada), 0, nombre, 0, 8);
+        //Aqui declaro una variable que me servira para saber el numero de preguntas y le doy el valor que da como resultado "numPregun(entrada)".
         int numpreguntas = numPregun(entrada);
         char respuestas[][] = new char[8][numpreguntas];
+
+        /* Aqui utilizo un system.arraycopy para copiar el array que me dara como resultado el metodo que llamo
+         * dentro de la misma linea "notas(entrada, numpreguntas, nombre)" y lo almaceno en el array 
+         * previamente declarado e instanciado "respuestas".
+         */
         System.arraycopy(notas(entrada, numpreguntas, nombre), 0, respuestas, 0, numpreguntas);
 
         
@@ -39,13 +51,24 @@ public class EjercicioObligatorio {
     }
 
     public static char[][] notas(Scanner entrada, int numero, String nombre[]) {
+
         char respuestas[][] = new char[8][numero];
+
         for(int i=0; i < 8; i++){
+
             System.out.print("Introduce las respuestas para el alumno "+ nombre[i]);
+
             for(int j=0;j<numero;j++){
-                System.out.print("Introduce la respuesta para le pregunta " + j);
+
+                System.out.print("Introduce la respuesta para la pregunta " + j);
                 String resultado = entrada.nextLine();
-                respuestas[i][j] = resultado.charAt(0); 
+
+                //while(resultado.equalsIgnoreCase("A")||resultado.equalsIgnoreCase("B")||resultado.equalsIgnoreCase("C")||resultado.equalsIgnoreCase("D")||resultado.equalsIgnoreCase("E")){
+                while(!resultado.matches("[a-e]")){
+                    System.out.print("Introduce la respuesta para la pregunta " + j);
+                    resultado = entrada.nextLine();
+                }
+                respuestas[i][j] = resultado.charAt(0);
             }
         }
         return respuestas;
