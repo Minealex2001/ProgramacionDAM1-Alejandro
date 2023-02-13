f6a1e61bb68b6f966bfd7d1a4c291a9a9ccb0988
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-02-13 19:27:51 +0100

    Quien lea esto me debe un besito

--- Segundo Trimestre/Tema 8/Ejercicios/EjB/B1/principal.java
+++ Segundo Trimestre/Tema 8/Ejercicios/EjB/B1/principal.java
@@ -1,21 +1,34 @@
 package EjB.B1;
 
 public class principal {
-    public static void main(String[] args) {
-        punto punto1 = new punto(5, 0);
-        punto punto2 = new punto(10, 10);
-        punto punto3 = new punto(-3, 7);
-    
-        System.out.println("Coordenadas Punto 1: (" + punto1.x + "," + punto1.y + ")");
-        System.out.println("Coordenadas Punto 2: (" + punto2.x + "," + punto2.y + ")");
-        System.out.println("Coordenadas Punto 3: (" + punto3.x + "," + punto3.y + ")");
-    
-        punto1.x = punto1.x + 1;
-        punto2.y = punto2.y - 1;
-        punto3.x = punto3.x * 2;
-    
-        System.out.println("Coordenadas Punto 1 modificadas: (" + punto1.x + "," + punto1.y + ")");
-        System.out.println("Coordenadas Punto 2 modificadas: (" + punto2.x + "," + punto2.y + ")");
-        System.out.println("Coordenadas Punto 3 modificadas: (" + punto3.x + "," + punto3.y + ")");
-      }
+
+  public static void main(String[] args) {
+    punto punto1 = new punto(5, 0);
+    punto punto2 = new punto(10, 10);
+    punto punto3 = new punto(-3, 7);
+
+    System.out.println(
+      "Coordenadas Punto 1: (" + punto1.x + "," + punto1.y + ")"
+    );
+    System.out.println(
+      "Coordenadas Punto 2: (" + punto2.x + "," + punto2.y + ")"
+    );
+    System.out.println(
+      "Coordenadas Punto 3: (" + punto3.x + "," + punto3.y + ")"
+    );
+
+    punto1.x = punto1.x + 1;
+    punto2.y = punto2.y - 1;
+    punto3.x = punto3.x * 2;
+
+    System.out.println(
+      "Coordenadas Punto 1 modificadas: (" + punto1.x + "," + punto1.y + ")"
+    );
+    System.out.println(
+      "Coordenadas Punto 2 modificadas: (" + punto2.x + "," + punto2.y + ")"
+    );
+    System.out.println(
+      "Coordenadas Punto 3 modificadas: (" + punto3.x + "," + punto3.y + ")"
+    );
+  }
 }
