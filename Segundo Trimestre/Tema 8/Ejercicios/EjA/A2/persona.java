8a39265c9be6d259ecd98c7fadc2bda984f8ab6b
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-02-13 18:47:18 +0100

    ChatGPT lo esta haciendo todo estoy muy triste

--- Segundo Trimestre/Tema 8/Ejercicios/EjA/A2/persona.java
+++ Segundo Trimestre/Tema 8/Ejercicios/EjA/A2/persona.java
@@ -1,7 +1,41 @@
 package EjA.A2;
 
-public class persona {
-    public persona(String nombre){
+class Persona {
+    private String dni;
+    private String nombre;
+    private String apellidos;
+    private int edad;
 
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
     }
 }
+
