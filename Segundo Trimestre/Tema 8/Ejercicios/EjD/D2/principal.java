142bf96a73a89420271952a43c18e678433ec043
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-02-15 15:43:02 +0100

    Es un copia y pega no os asusteis

---
+++ Segundo Trimestre/Tema 8/Ejercicios/EjD/D2/principal.java
@@ -0,0 +1,57 @@
+package EjD.D2;
+
+import java.util.Scanner;
+
+public class principal {
+
+  public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+    Persona persona1 = new Persona();
+    Persona persona2 = new Persona();
+
+    System.out.println("Datos de la primera persona:");
+    System.out.print("DNI: ");
+    persona1.setDni(entrada.nextLine());
+    System.out.print("Nombre: ");
+    persona1.setNombre(entrada.nextLine());
+    System.out.print("Apellidos: ");
+    persona1.setApellidos(entrada.nextLine());
+    System.out.print("Edad: ");
+    persona1.setEdad(entrada.nextInt());
+
+    entrada.nextLine();
+
+    System.out.println("\nDatos de la segunda persona:");
+    System.out.print("DNI: ");
+    persona2.setDni(entrada.nextLine());
+    System.out.print("Nombre: ");
+    persona2.setNombre(entrada.nextLine());
+    System.out.print("Apellidos: ");
+    persona2.setApellidos(entrada.nextLine());
+    System.out.print("Edad: ");
+    persona2.setEdad(entrada.nextInt());
+
+    System.out.println("\nInformación de las personas:");
+    if (persona1.getEdad() >= 18) {
+      System.out.println(
+        persona1.getNombre() +
+        " " +
+        persona1.getApellidos() +
+        " con DNI " +
+        persona1.getDni() +
+        " es mayor de edad"
+      );
+    } else {
+      System.out.println(
+        persona1.getNombre() +
+        " " +
+        persona1.getApellidos() +
+        " con DNI " +
+        persona1.getDni() +
+        " no es mayor de edad"
+      );
+    }
+
+    entrada.close();
+  }
+}
---
+++ Segundo Trimestre/Tema 8/Ejercicios/EjD/D3/principal.java
@@ -0,0 +1,52 @@
+package EjD.D3;
+
+class Persona {
+
+  public class Main {
+
+    public static void main(String[] args) {
+      rectangulo rectangulo1 = new rectangulo(0, 0, 5, 5);
+      rectangulo rectangulo2 = new rectangulo(7, 9, 2, 3);
+
+      System.out.println("Rectángulo 1:");
+      System.out.println(
+        "Coordenadas: (" +
+        rectangulo1.x1 +
+        ", " +
+        rectangulo1.y1 +
+        "), (" +
+        rectangulo1.x2 +
+        ", " +
+        rectangulo1.y2 +
+        ")"
+      );
+      System.out.println("Perímetro: " + rectangulo1.getPerimetro());
+      System.out.println("Área: " + rectangulo1.getArea());
+
+      System.out.println("\nRectángulo 2:");
+      System.out.println(
+        "Coordenadas: (" +
+        rectangulo2.x1 +
+        ", " +
+        rectangulo2.y1 +
+        "), (" +
+        rectangulo2.x2 +
+        ", " +
+        rectangulo2.y2 +
+        ")"
+      );
+      System.out.println("Perímetro: " + rectangulo2.getPerimetro());
+      System.out.println("Área: " + rectangulo2.getArea());
+
+      rectangulo1.setX1(1);
+      rectangulo1.setY1(1);
+      rectangulo1.setX2(6);
+      rectangulo1.setY2(6);
+
+      rectangulo2.setX1(3);
+      rectangulo2.setY1(3);
+      rectangulo2.setX2(4);
+      rectangulo2.setY2(4);
+    }
+  }
+}
